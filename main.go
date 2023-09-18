2b5ef2d3770be674e1fcff66142487cbc428b9a8
SeyyedMehdiHoseini <mehdihoseini@orginal.com>
2023-09-18 22:58:11 +0430

    update

--- main.go
+++ main.go
@@ -116,27 +116,26 @@ func deleteFromCSV(ID uint) {
 	}
 	csvWriter.Flush()
 }
+func updateFromCSV(ID uint) {
+	articles, err := GetAllFromCSV()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-// func updateFromCSV(ID uint) {
-// 	articles, err := GetAllFromCSV()
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
+	articles = updateSpificArticle(ID, articles)
+	file, err := os.OpenFile("data.csv", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
 
-// 	articles = updateSpificArticle(ID, articles)
-// 	file, err := os.OpenFile("data.csv", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	defer file.Close()
+	// csvWriter := csv.NewWriter(file)
+	// if err := csvWriter.Write(ArticlesToRecords(articles));err != nil{
+	// 	fmt.Println(err)
+	// }
+	// csvWriter.Flush()
 
-// 	csvWriter := csv.NewWriter(file)
-// 	// if err := csvWriter.Write(ArticlesToRecords(articles));err != nil{
-// 	// 	fmt.Println(err)
-// 	// }
-// 	// csvWriter.Flush()
-
-// }
+}
 
 func GetAllFromCSV() ([]Article, error) {
 	// os.O_APPEND|os.O_CREATE|os.O_WRONLY,
